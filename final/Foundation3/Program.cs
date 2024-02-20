302c675313cdcc062aabb88104fdc3e99dc78748
malloryq <malloryq96@gmail.com>
2024-02-20 16:46:49 -0600

    commit

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -1,9 +1,150 @@
 using System;
 
-class Program
+public class Address
+{
+    private string streetAddress;
+    private string city;
+    private string state;
+    private string country;
+
+    public Address(string streetAddress, string city, string state, string country)
+    {
+        this.streetAddress = streetAddress;
+        this.city = city;
+        this.state = state;
+        this.country = country;
+    }
+
+    public string GetFullAddress()
+    {
+        return $"{streetAddress}, {city}, {state}, {country}";
+    }
+
+    public bool IsInUSA()
+    {
+        return country.Equals("USA", StringComparison.OrdinalIgnoreCase);
+    }
+}
+
+public class Event
+{
+    private string _title;
+    private string _description;
+    private DateTime _date;
+    private string _time;
+    private Address _address;
+
+    public Event(string title, string description, DateTime date, string time, Address address)
+    {
+        _title = title;
+        _description = description;
+        _date = date;
+        _time = time;
+        _address = address;
+    }
+
+    public virtual string GetStandardDetails()
+    {
+        return $"Title: {_title}\nDescription: {_description}\nDate: {_date.ToShortDateString()}\nTime: {_time}\nAddress: {_address.GetFullAddress()}";
+    }
+
+    public virtual string GetFullDetails()
+    {
+        return GetStandardDetails();
+    }
+
+    public virtual string GetShortDescription()
+    {
+        return $"Event Type: Standard\nTitle: {_title}\nDate: {_date.ToShortDateString()}";
+    }
+}
+
+public class Lecture : Event
+{
+    private string _speaker;
+    private int _capacity;
+
+    public Lecture(string title, string description, DateTime date, string time, Address address, string speaker, int capacity)
+        : base(title, description, date, time, address)
+    {
+        _speaker = speaker;
+        _capacity = capacity;
+    }
+
+    public override string GetFullDetails()
+    {
+        return base.GetStandardDetails() + $"\nEvent Type: Lecture\nSpeaker: {_speaker}\nCapacity: {_capacity}";
+    }
+
+    public override string GetShortDescription()
+    {
+        return $"Event Type: Lecture\nTitle: {_title}\nDate: {_date.ToShortDateString()}";
+    }
+}
+
+public class Reception : Event
+{
+    private string _rsvpEmail;
+
+    public Reception(string title, string description, DateTime date, string time, Address address, string rsvpEmail)
+        : base(title, description, date, time, address)
+    {
+        _rsvpEmail = rsvpEmail;
+    }
+
+    public override string GetFullDetails()
+    {
+        return base.GetStandardDetails() + $"\nEvent Type: Reception\nRSVP Email: {_rsvpEmail}";
+    }
+
+    public override string GetShortDescription()
+    {
+        return $"Event Type: Reception\nTitle: {_title}\nDate: {_date.ToShortDateString()}";
+    }
+}
+
+public class OutdoorGathering : Event
+{
+    private string _weatherStatement;
+
+    public OutdoorGathering(string title, string description, DateTime date, string time, Address address, string weatherStatement)
+        : base(title, description, date, time, address)
+    {
+        _weatherStatement = weatherStatement;
+    }
+
+    public override string GetFullDetails()
+    {
+        return base.GetStandardDetails() + $"\nEvent Type: Outdoor Gathering\nWeather Statement: {_weatherStatement}";
+    }
+
+    public override string GetShortDescription()
+    {
+        return $"Event Type: Outdoor Gathering\nTitle: {_title}\nDate: {_date.ToShortDateString()}";
+    }
+}
+
+class Program3
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        Address address1 = new Address("123 Main St", "City", "State", "Country");
+        Address address2 = new Address("456 Elm St", "Town", "Province", "Country");
+
+        Event lecture = new Lecture("Lecture on AI", "Introduction to Artificial Intelligence", new DateTime(2024, 2, 20), "10:00 AM", address1, "Dr. Smith", 50);
+        Event reception = new Reception("Company Anniversary Reception", "Celebrating 10 Years of Success", new DateTime(2024, 3, 15), "7:00 PM", address2, "rsvp@example.com");
+        Event outdoorGathering = new OutdoorGathering("Summer Picnic", "Annual Company Picnic", new DateTime(2024, 6, 1), "12:00 PM", address1, "Sunny weather expected");
+
+        Console.WriteLine("Lecture Details:");
+        Console.WriteLine(lecture.GetFullDetails());
+        Console.WriteLine("\nReception Details:");
+        Console.WriteLine(reception.GetFullDetails());
+        Console.WriteLine("\nOutdoor Gathering Details:");
+        Console.WriteLine(outdoorGathering.GetFullDetails());
+
+        Console.WriteLine("\nShort Descriptions:");
+        Console.WriteLine(lecture.GetShortDescription());
+        Console.WriteLine(reception.GetShortDescription());
+        Console.WriteLine(outdoorGathering.GetShortDescription());
     }
 }
--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,128 @@
 using System;
 
-class Program
+public abstract class Activity
+{
+    protected DateTime _date;
+    protected int _minutes;
+
+    public Activity(DateTime date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+
+    public abstract double GetDistance();
+
+    public abstract double GetSpeed();
+
+    public abstract double GetPace();
+
+    public virtual string GetSummary()
+    {
+        return $"{_date.ToShortDateString()} {GetType().Name} ({_minutes} min)";
+    }
+}
+
+public class Running : Activity
+{
+    private double _distance;
+
+    public Running(DateTime date, int minutes, double distance) : base(date, minutes)
+    {
+        _distance = distance;
+    }
+
+    public override double GetDistance()
+    {
+        return _distance;
+    }
+
+    public override double GetSpeed()
+    {
+        return _distance / (_minutes / 60.0);
+    }
+
+    public override double GetPace()
+    {
+        return _minutes / _distance;
+    }
+
+    public override string GetSummary()
+    {
+        return $"{base.GetSummary()} - Distance: {_distance} miles, Speed: {GetSpeed()} mph, Pace: {GetPace()} min/mile";
+    }
+}
+
+public class Cycling : Activity
+{
+    private double _speed;
+
+    public Cycling(DateTime date, int minutes, double speed) : base(date, minutes)
+    {
+        _speed = speed;
+    }
+
+    public override double GetSpeed()
+    {
+        return _speed;
+    }
+
+    public override double GetDistance()
+    {
+        return _speed * (_minutes / 60.0);
+    }
+
+    public override double GetPace()
+    {
+        return 60 / _speed;
+    }
+
+    public override string GetSummary()
+    {
+        return $"{base.GetSummary()} - Distance: {GetDistance()} miles, Speed: {_speed} mph, Pace: {GetPace()} min/mile";
+    }
+}
+
+public class Swimming : Activity
+{
+    private int _laps;
+
+    public Swimming(DateTime date, int minutes, int laps) : base(date, minutes)
+    {
+        _laps = laps;
+    }
+
+    public override double GetDistance()
+    {
+        return _laps * 50 / 1000.0;
+    }
+
+    public override double GetSpeed()
+    {
+        return (_laps * 50 / 1000.0) / (_minutes / 60.0);
+    }
+
+    public override double GetPace()
+    {
+        return _minutes / (_laps * 50 / 1000.0);
+    }
+
+    public override string GetSummary()
+    {
+        return $"{base.GetSummary()} - Distance: {GetDistance()} km, Speed: {GetSpeed()} kph, Pace: {GetPace()} min/km";
+    }
+}
+
+class Program4
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        Activity running = new Running(new DateTime(2024, 2, 15), 30, 3.0);
+        Activity cycling = new Cycling(new DateTime(2024, 2, 15), 30, 9.7);
+        Activity swimming = new Swimming(new DateTime(2024, 2, 15), 30, 5);
+
+        Console.WriteLine(running.GetSummary());
+        Console.WriteLine(cycling.GetSummary());
+        Console.WriteLine(swimming.GetSummary());
     }
 }
